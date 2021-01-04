934f816ce04df9426f4c25ec1c74fc0f8647ffe6
stefan <stefan.s27.08@gmail.comD>
2021-01-04 17:00:48 +0100

    add databinding

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -10,13 +10,12 @@ import android.util.Size
 import android.view.Surface
 import android.view.TextureView
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.camera.databinding.ActivityMainBinding
 import com.example.camera.tflite.Classifier
-import com.example.camera.utils.AutoFitTextureView
 import com.example.camera.utils.CameraUtils
 import com.example.camera.utils.TAG
 
@@ -26,35 +25,34 @@ private const val RESULT_FORMAT = "%s %.2f"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val cameraThread = HandlerThread("Camera Thread").apply { start() }
     private val imageReaderThread = HandlerThread("ImageReader Thread").apply { start() }
-
-    private lateinit var textureView: AutoFitTextureView
-    private lateinit var txtResult1: TextView
-    private lateinit var txtResult2: TextView
-    private lateinit var txtResult3: TextView
-
     private val cameraUtils by lazy {
         CameraUtils(
             applicationContext = this.applicationContext,
             cameraHandler = Handler(cameraThread.looper),
             imageReaderHandler = Handler(imageReaderThread.looper),
             eventListener = object : CameraUtils.EventListener {
-                override fun onPreviewSizeSelected(size: Size) {
-                    textureView.setAspectRatio(size.height, size.width)
-                    textureView.surfaceTexture.setDefaultBufferSize(size.width, size.height)
-                    Log.d(
-                        TAG,
-                        "Texture View preview size after applying values: ${textureView.width} x ${textureView.height}"
-                    )
+                override fun onError(text: String) =
+                    Toast.makeText(this@MainActivity, text, LENGTH_SHORT).show()
+
+                override fun provideTextureViewSurface() =
+                    Surface(binding.textureView.surfaceTexture)
+
+                override fun onProcessingResult(result: MutableList<Classifier.Recognition>) {
+                    binding.txtResult1.text = RESULT_FORMAT.format(result[0].title, result[0].confidence * 100)
+                    binding.txtResult2.text = RESULT_FORMAT.format(result[1].title, result[1].confidence * 100)
+                    binding.txtResult3.text = RESULT_FORMAT.format(result[2].title, result[2].confidence * 100)
                 }
 
-                override fun onError(text: String) = Toast.makeText(this@MainActivity, text, LENGTH_SHORT).show()
-                override fun provideTextureViewSurface() = Surface(textureView.surfaceTexture)
-                override fun onProcessingResult(result: MutableList<Classifier.Recognition>) {
-                    txtResult1.text = RESULT_FORMAT.format(result[0].title, result[0].confidence * 100)
-                    txtResult2.text = RESULT_FORMAT.format(result[1].title, result[1].confidence * 100)
-                    txtResult3.text = RESULT_FORMAT.format(result[2].title, result[2].confidence * 100)
+                override fun onPreviewSizeSelected(size: Size) {
+                    binding.textureView.setAspectRatio(size.height, size.width)
+                    binding.textureView.surfaceTexture.setDefaultBufferSize(size.width, size.height)
+                    Log.d(
+                        TAG, "Texture View preview size after applying values: " +
+                                "${binding.textureView.width} x ${binding.textureView.height}"
+                    )
                 }
             }
         )
@@ -62,17 +60,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         checkForPermissionsAndInitViews()
-        txtResult1 = findViewById(R.id.txt_result_1)
-        txtResult2 = findViewById(R.id.txt_result_2)
-        txtResult3 = findViewById(R.id.txt_result_3)
     }
 
 
     private fun initViews() {
-        textureView = findViewById(R.id.texture_view)
-        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+        binding.textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
             override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) =
                 cameraUtils.onTextureViewAvailable(windowManager.defaultDisplay.rotation)
 
@@ -123,6 +118,7 @@ class MainActivity : AppCompatActivity() {
 
     fun saveImage(view: View) {
         cameraUtils.saveImage()
+        Toast.makeText(this, "Image saved.", Toast.LENGTH_SHORT).show()
     }
 
 }
