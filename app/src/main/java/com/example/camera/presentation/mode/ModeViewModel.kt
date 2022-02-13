da9c06460889743d3a2b48a756950a12502b3d97
stefans2708 <stefan.s27.08@gmail.com>
2022-02-13 10:48:17 +0100

    mode-chooser screen with tutorial image

--- app/src/main/java/com/example/camera/presentation/mode/ModeViewModel.kt
+++ app/src/main/java/com/example/camera/presentation/mode/ModeViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.camera.presentation.mode
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.example.camera.R
 import com.example.camera.presentation.base.BaseViewModel
 
 class ModeViewModel : BaseViewModel<ModeViewModel.ModeAction>() {
@@ -9,7 +13,23 @@ class ModeViewModel : BaseViewModel<ModeViewModel.ModeAction>() {
         DETECTION
     }
 
-    fun onCarClassificationSelected() = setAction(ModeAction.CAR_BRAND_CLASSIFICATION)
+    private val _selectedImageLive = MutableLiveData(R.drawable.drawing_with_classifications2)
 
-    fun onObjectDetectionSelected() = setAction(ModeAction.DETECTION)
+    val selectedImageLive: LiveData<Int> get() = _selectedImageLive
+    val classificationSelected: LiveData<Boolean> =
+        Transformations.map(_selectedImageLive) { it == R.drawable.drawing_with_classifications2 }
+    val detectionSelected: LiveData<Boolean> =
+        Transformations.map(_selectedImageLive) { it == R.drawable.drawing_with_detections3 }
+
+    fun onCarClassificationSelected() =
+        _selectedImageLive.postValue(R.drawable.drawing_with_classifications2)
+
+    fun onObjectDetectionSelected() =
+        _selectedImageLive.postValue(R.drawable.drawing_with_detections3)
+
+    fun onProceedButtonClick() =
+        setAction(
+            if (classificationSelected.value == true) ModeAction.CAR_BRAND_CLASSIFICATION
+            else ModeAction.DETECTION
+        )
 }
